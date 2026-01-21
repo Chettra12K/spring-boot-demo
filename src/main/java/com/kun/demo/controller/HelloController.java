33923e798695b765b0f89cea51c74e036746167d
Chettra12k <kunchettra500@gmail.com>
2026-01-21 23:39:01 +0700

    First Spring Boot Hello World API

---
+++ src/main/java/com/kun/demo/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.kun.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World! សួរស្តី Kun សូមស្វាគមន៏មកកាន់ Spring Boot";
+    }
+    //bonus: API មួយទៀត
+    @GetMapping("/api/greeting")
+    public String greeting(){
+        return "សួស្តី! នេះជា Rest API ដំបូងរបស់អ្នកនៅឆ្នាំ 2027";
+    }
+    @GetMapping("/api/time")
+    public String getTime(){
+        return "ម៉ោងបច្ចុបន្នៈ "+java.time.LocalDateTime.now();
+    }
+    @GetMapping("/api/name/{name}")
+    public String sayHi(@PathVariable String name){
+        return "សួស្តី " + name + "! រីករាយណាស់ដែលបានជួបអ្នក";
+    }
+
+}
